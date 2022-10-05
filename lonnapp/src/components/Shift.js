8229207affed02d725563eeb8f0c857f746268cb
Rius2g <marius.mollerhansen@hotmail.com>
2022-10-05 23:18:02 +0200

    small change

--- lonnapp/src/components/Shift.js
+++ lonnapp/src/components/Shift.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Shifts = ({Shift}) => {
     return (
-        <div date={Shift.id} key={Shift.id + Shift.date} name="Shift" value={Shift.id} 
+        <div id={Shift.id} key={Shift.id + Shift.date} name="Shift" value={Shift.id} 
         direction="row" spacing={3} alignItems="center" justifyContent="center">
             {Shift.date}
             {' '}
